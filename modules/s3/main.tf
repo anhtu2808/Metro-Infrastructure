9f6f696d7dd384128794cd2b757a14cc86f9132a
Đặng Mai Anh Tú <tudmase182951@fpt.edu.vn>
2025-06-19 23:09:20 +0700

    Public get policy

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -20,3 +20,31 @@ resource "aws_s3_bucket_cors_configuration" "this" {
     max_age_seconds = 3000
   }
 }
+
+# Allow public read access
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  depends_on = [aws_s3_bucket_public_access_block.this]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.this.arn}/*"
+      }
+    ]
+  })
+}
